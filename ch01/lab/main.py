754533102c2d8fa0522b8ec85274d2e0469c79c2
Riya Nobi <rnobi1@binghamton.edu>
2023-02-01 17:16:14 -0500

    ch01 lab

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,20 @@
+import random
 
+#Part A
+weeks = 16
+print(weeks, type(weeks))
+classes = 5
+print(classes, type(classes))
+tuition = 6000
+print(tuition, type(tuition))
+cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week)
+classes_per_week = 4
+print(classes_per_week, type(classes_per_week))
+cost_per_class = (cost_per_week / classes_per_week)
+print(f"The cost of each class is ${cost_per_class}.")
+
+#Part B
+animals = ["tiger", "monkey", "shark", "snake", "robin", "butterfly", "goose", "slug"]
+random_animal = random.choice(animals)
+print(random_animal)
